c5d2d46cc0d9ef1ba7834ed9098533bddca48dad
José Marcio <jose.souza.24716@a.fecaf.com.br>
2022-09-28 16:06:40 -0300

    questions

---
+++ Questions/app.js
@@ -0,0 +1,29 @@
+//usando seletores dentro do elemento
+//SELECTORS
+const questions = document.querySelectorAll('.question')
+
+//EVENTS
+questions.forEach(function (question) {
+  const btn = question.querySelector('.question-btn')
+  btn.addEventListener('click', function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove('show-text')
+      }
+    })
+    question.classList.toggle('show-text')
+  })
+})
+
+// percorrendo o dom
+//SELECTORS
+/* const btns = document.querySelectorAll('.question-btn')
+
+//EVENTS
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    const question = e.currentTarget.parentElement.parentElement
+    question.classList.toggle('show-text')
+  })
+})
+ */
